ebf4e6cfce8e2db9b139b689cddf95f0a4727b39
villva-vinoth <vinothpukal6580@gmail.com>
2024-09-03 19:32:54 +0530

    URl changed

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
-export const API_URL = 'http://localhost:4000'
+// export const API_URL = 'http://localhost:4000'
 
-// export const API_URL = "https://naturally-eminent-cub.ngrok-free.app";
+export const API_URL = "https://naturally-eminent-cub.ngrok-free.app";
 
 export const LOGIN = API_URL + "/api/auth/login";
 
